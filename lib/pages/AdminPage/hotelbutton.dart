1a6457a6671a4bcecf3bf431e1c1fb5e62ebea55
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2024-12-21 01:13:49 +0600

    adminpage created

---
+++ lib/pages/AdminPage/hotelbutton.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+class HotelButton extends StatelessWidget {
+  const HotelButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    appBar: AppBar(
+      backgroundColor: Colors.white,
+      leading: IconButton(onPressed: (){
+        Navigator.pop(context);
+      }, icon: Icon(Icons.arrow_back)),
+
+      title: Text('Home'),
+    ),
+      body: Column(
+
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 73, vertical: 60),
+            child: Text(
+              'Select your option', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold,),),
+          ),
+
+          SizedBox(width: 316,child:
+          ElevatedButton(onPressed: (){},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF19A7FE),
+              padding: EdgeInsets.symmetric( vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+             child: Text('Add Room',style: TextStyle(fontSize: 14,color: Colors.white),),
+
+          ),
+          ),
+
+         SizedBox(height: 25),
+          SizedBox(width: 316,child:
+          ElevatedButton(onPressed: (){},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF19A7FE),
+              padding: EdgeInsets.symmetric( vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text('View Room',style: TextStyle(fontSize: 14,color: Colors.white),),
+
+          ),
+          ),
+          SizedBox(height: 25),
+          SizedBox(width: 316,child:
+          ElevatedButton(onPressed: (){},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF19A7FE),
+              padding: EdgeInsets.symmetric( vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text('Delete Room',style: TextStyle(fontSize: 14,color: Colors.white),),
+
+          ),
+          ),
+
+          SizedBox(height: 25),
+          SizedBox(width: 316,child:
+          ElevatedButton(onPressed: (){},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF19A7FE),
+              padding: EdgeInsets.symmetric( vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text('Update Room',style: TextStyle(fontSize: 14,color: Colors.white),),
+
+          ),
+          ),
+
+          SizedBox(height: 25),
+          SizedBox(width: 316,child:
+          ElevatedButton(onPressed: (){},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF19A7FE),
+              padding: EdgeInsets.symmetric(vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text('Manage Booking',style: TextStyle(fontSize: 14,color: Colors.white),),
+
+          ),
+          ),
+        ],
+
+      ),
+     bottomNavigationBar:  BottomNavigationBar(
+          backgroundColor: Color(0xFFE4E4E4),
+          currentIndex: 1,
+
+          items:[
+
+            BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.hotel_rounded),label: 'Hotel'),
+            BottomNavigationBarItem(icon: Icon(Icons.restaurant_rounded),label: 'Restaurant')
+          ]
+      ),
+
+
+    );
+  }
+}
